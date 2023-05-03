8b756840a8d9d8078181a6211f3e476980adfc28
Washim-Akram <muhammadwashimakram@gmail.com>
2023-05-03 21:13:11 +0600

    Added chef.json to server and create a get request to serve data in the UI

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const chef = require('./data/chef.json');
+
 app.get('/', (req, res) => {
     res.send('The Tokyo Food Server is Running.')
 });
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
 
 app.listen(port, () => {
     console.log(`The Tokyo Food Server Listening on Port: ${port}`);
